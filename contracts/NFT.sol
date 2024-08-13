d79a639c39f32c3ce4a726cfe145f016eec5f1c5
villas6 <villas666here@gmail.com>
2024-08-13 08:34:34 +0000

    feat: initiated the project

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 
 contract TestNFT is ERC721, ERC721Burnable {
     constructor()
-        ERC721("56","6")
+        ERC721("4","5")
     {}
 
     function safeMint(address to, uint256 tokenId) public {
