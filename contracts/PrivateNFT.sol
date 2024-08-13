6ca8897312508ca17461ee10707f6c4d5e28f29e
villas6 <villas666here@gmail.com>
2024-08-13 08:40:41 +0000

    feat: initiated the project

--- contracts/PrivateNFT.sol
+++ contracts/PrivateNFT.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract PrivateNFT is ERC721, ERC721Burnable, Ownable {
     constructor(address initialOwner)
-        ERC721("13","2")
+        ERC721("1","2")
         Ownable(initialOwner)
     {}
 
